867910aeae8d2c363695b7d88467043091216194
mavalver <mvalver@gmail.com>
2016-10-13 23:00:00 +0200

    prueba de commit

--- prueba-repo-swith-git/ViewController.swift
+++ prueba-repo-swith-git/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // PRUEBA DE MODIFICACIÓN DE GIT
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
